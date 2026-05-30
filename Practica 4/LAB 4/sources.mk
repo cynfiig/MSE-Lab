93496b1667ae704920d70b4efedc9eb6342caeae
ccayetanofig <ccayetanofig@gmail.com>
2026-05-29 20:55:22 -0700

    Add Practica 3 and Practica 4

---
+++ Practica 4/LAB 4/sources.mk
@@ -0,0 +1,22 @@
+# This is a makefile for our source files 
+SRC_DIR = .
+INCLUDE_DIR = .
+CMSIS_DIR = ./CMSIS
+
+# List all your source files here
+SRCS = \
+$(SRC_DIR)/main.c \
+$(SRC_DIR)/stm32.startup.c \
+$(SRC_DIR)/system_stm32f4xx.c \
+$(SRC_DIR)/gpio_driver.c \
+$(SRC_DIR)/tim_driver.c \
+$(SRC_DIR)/adc_driver.c \
+$(SRC_DIR)/uart.c \
+$(SRC_DIR)/serial.c \
+$(SRC_DIR)/sensor.c \
+$(SRC_DIR)/utils.c    
+# List all your include directories here
+INCLUDES = \
+-I$(INCLUDE_DIR) \
+-I$(CMSIS_DIR) \
+-I$(CMSIS_DIR)/STM32F4xx
